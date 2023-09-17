e039f848691349ac9648f282b3c76e0366923b4f
str1der <tim.kuiper@protonmail.com>
2023-09-17 12:46:03 +0200

    updates

--- python_mouseclicker/code/code.py
+++ python_mouseclicker/code/code.py
@@ -20,7 +20,7 @@ def move_rel(x,y,d):
     pyautogui.moveRel(x, y, duration = d)
 
 list = [1, 2, 3, 4]
-for z in range(10000000):
+for z in range(1000000000):
     move_to(960,540,0.1) # go to center
     random_element = random.choice(list) # pick random element from list
 
@@ -28,43 +28,36 @@ for z in range(10000000):
         move_rel(50,0,0.1) # go right
         current_pos = pyautogui.position() # get current pos
         pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
     elif random_element == 2:
         move_rel(0,50,0.1) # go down
         current_pos = pyautogui.position() # get current pos
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
         pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
     elif random_element == 3:
         move_rel(-50,0,0.1) # go left
         current_pos = pyautogui.position() # get current pos
         pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
     elif random_element == 4:
         move_rel(0,-100,0.1) # go up
         current_pos = pyautogui.position() # get current pos
         pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-        pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
-    
     # every 1000 iterations press f1
     if z % 1000 == 0:
         pyautogui.hotkey("F1")
+    # every 250 iterations perform a long walk to decrease chances of getting stuck near edge of map
+    if z % 250 == 0:
+        if random_element == 1:
+            move_rel(500,0,0.1) # go right
+            current_pos = pyautogui.position() # get current pos
+            pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
+        elif random_element == 2:
+            move_rel(0,500,0.1) # go down
+            current_pos = pyautogui.position() # get current pos
+            pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
+        elif random_element == 3:
+            move_rel(-500,0,0.1) # go left
+            current_pos = pyautogui.position() # get current pos
+            pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
+        elif random_element == 4:
+            move_rel(0,-500,0.1) # go up
+            current_pos = pyautogui.position() # get current pos
+            pyautogui.click(current_pos.x, current_pos.y, clicks=1, duration=0.1) # click
