b46f42d0255c8d4fc7256265abc375bf6b88d6e9
str1der <tim.kuiper@protonmail.com>
2024-04-06 14:18:09 +0200

    clean repo

--- python_crashcourse/ch7_73.py
+++
@@ -1,7 +0,0 @@
-number = input("Please enter a number: ")
-number = int(number)
-
-if number % 10 == 0:
-    print("Number is a multiple of 10")
-else:
-    print("Number is not a multiple of 10")
--- python_crashcourse/ch7_74.py
+++
@@ -1,9 +0,0 @@
-prompt = "\nPlease enter your pizza topping:"
-prompt += "\n(Enter 'quit' when you are finished.)"
-
-while True:
-    pizza_topping = input(prompt)
-    if pizza_topping == 'quit':
-        break
-    else:
-        print(f"Adding topping {pizza_topping} to the pizza")
