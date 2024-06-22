20fe2c58a9aac7e4c42ed278538bfd1235c55c93
Tim Kuiper <tim.kuiper@protonmail.com>
2024-06-22 14:30:00 +0200

    succesfull 1st working example of backtesting, needs more still

--- test/backtest/backtest.py
+++ test/backtest/backtest.py
@@ -16,21 +16,22 @@ import itertools
 ## general vars
 
 asset_dict = {}
+# asset_pairs = ['XXBTZUSD', 'XXRPZUSD', 'XETHZUSD', 'ADAUSD', 'SOLUSD']
 asset_pairs = ['XXBTZUSD']
 pd.options.display.max_rows = 999
 pd.options.display.max_columns = 8
 api_url = "https://api.kraken.com"
-tg_token = os.environ['telegram_token']
-list_1h = []
-list_4h = []
-list_24h = []
-start_list_24h = [] # use this list in combination with the regular 24h list in order to execute the 24h block without waiting a full day
 loop_time_seconds = 14400
-rsi_lower_boundary = 35
-rsi_upper_boundary = 65
+rsi_lower_boundary = 25
+rsi_upper_boundary = 75
 interval_time_minutes = 240 # 4h timeframe
+# interval_time_minutes = 1440 # 1d timeframe
+# interval_time_minutes = 10080 # 1w timeframe
+# interval_time_minutes = 60 # 1h timeframe
+#interval_time_minutes = 15 # 15m timeframe
 data_dict = {}
 balance_usd = 5000
+order_size = 400
 
 # functions
 def get_asset_vars():
@@ -56,8 +57,6 @@ def get_asset_vars():
       api_sec = os.environ['api_sec_env_eth']
       api_key = os.environ['api_key_env_eth']
     return [asset_code, api_sec, api_key]
-
-
 
 def get_kraken_signature(urlpath, data, secret):
     postdata = urllib.parse.urlencode(data)
@@ -129,11 +128,10 @@ def get_rsi(period: int = 14, round_rsi: bool = True):
     return np.round(rsi, 2) if round_rsi else rsi
 
 def get_asset_amount():
-    buy_amount_usd = 200
     asset_amount_list = []
     close_list = get_close().tolist()
     for i in close_list:
-      asset_amount_list.append(float(buy_amount_usd)/float(i))
+      asset_amount_list.append(float(order_size)/float(i))
     return asset_amount_list
 
 for asset_pair in asset_pairs:
@@ -151,56 +149,81 @@ for asset_pair in asset_pairs:
   amount_list_temp = []
   holdings_list = []
   price_bought_list = []
-  for (time, macd, rsi, close, amount) in zip(time_list, macd_list, rsi_list, close_list, amount_list):
-    #print(f"{time}, {macd}, {rsi}, {close}, {amount}")
+  for (times, macd, rsi, close, amount) in zip(time_list, macd_list, rsi_list, close_list, amount_list):
+    #print(f"{times}, {macd}, {rsi}, {close}, {amount}")
     # print(f"Balance USD: {balance_usd}")
     is_macd_float = isinstance(macd,float)
-    if 0 < rsi < 100 and is_macd_float:
-      print(f"rsi {rsi} and macd {macd}")
+    if 0 < rsi < 100 and macd == macd: # NaN type is always not equal, also to itself
+      # print(f"rsi {rsi} and macd is a float: {macd}")
       if len(rsi_list_temp) == 1:
+        #print(f"Length RSI temp list: {rsi_list_temp}")
         if rsi_list_temp[0] < rsi_lower_boundary:
+          #print(f"RSI in RSI temp list is lower than RSI lower boundary {rsi_lower_boundary}: {rsi_list_temp[0]}, keeping value in list")
           rsi = rsi_list_temp[0]
         elif rsi_list_temp[0] > rsi_upper_boundary:
+          #print(f"RSI in RSI temp list is higher than RSI upper boundary {rsi_upper_boundary}: {rsi_list_temp[0]}, keeping value in list")
           rsi = rsi_list_temp[0]
         else:
+          #print(f"RSI is between {rsi_lower_boundary} and {rsi_upper_boundary}: {rsi} so we're clearing it from the RSI temp list")
           rsi_list_temp.clear()
           rsi_list_temp.append(rsi)
       elif len(rsi_list_temp) == 0:
+        #print(f"RSI temp list empty: {rsi_list_temp}, appending rsi {rsi} to list")
         rsi_list_temp.append(rsi)
       if rsi < rsi_lower_boundary and len(macd_list_temp) < 3:
+        #print(f"RSI: {rsi} and length of macd list temp lower than 3: {macd_list_temp}")
         macd_list_temp.append(macd)
       if rsi < rsi_lower_boundary and len(macd_list_temp) >= 3:
+        #print(f"RSI: {rsi} and macd list temp: {macd_list_temp}")
         if macd_list_temp[-3] < macd_list_temp[-2] < macd_list_temp[-1]:
-          print(f"Buy asset")
+          #print(f"MACD in upward trend, buying asset")
           holdings_list.append(amount)
-          print(f"Holdings list: {holdings_list}")
+          # usd_bought = sum(holdings_list) * close
+          balance_usd = balance_usd - order_size
+          print(f"USD balance after buy: {balance_usd}")
+          #print(f"Holdings list: {holdings_list}")
           price_bought_list.append(close)
+          #print(f"Price bought list: {price_bought_list}")
           macd_list_temp.clear()
           rsi_list_temp.clear()
         else:
+          #print(f"MACD list temp not in upward trend, appending {macd} to macd list temp")
           macd_list_temp.append(macd)
+          #print(f"Appended macd {macd} to macd list temp: {macd_list_temp}")
       elif rsi > rsi_upper_boundary and len(macd_list_temp) < 3:
+        #print(f"RSI {rsi} but macd list temp has not enough length: {macd_list_temp}")
         macd_list_temp.append(macd)
+        #print(f"Appended {macd} to macd list temp, macd list temp: {macd_list_temp}")
       elif rsi > rsi_upper_boundary and len(macd_list_temp) >= 3:
+        #print(f"RSI {rsi} and macd list temp > 3: {macd_list_temp}")
         if macd_list_temp[-3] > macd_list_temp[-2] > macd_list_temp[-1]:
+          #print(f"Downward macd trend for macd_list_temp: {macd_list_temp}, selling asset if we have any")
           if float(sum(holdings_list)) > 0:
+            #print(f"Holdings greater than 0: {sum(holdings_list)}")
             price_bought_avg = sum(price_bought_list) / len(price_bought_list)
+            #print(f"Avg price bought: {price_bought_avg}")
             if close > price_bought_avg:
               usd_sold = sum(holdings_list) * close
               balance_usd = balance_usd + usd_sold
-              print(f"Balance USD: {balance_usd}")
+              print(f"USD balance after sell: {balance_usd}")
               macd_list_temp.clear()
               rsi_list_temp.clear()
               price_bought_list.clear()
+              holdings_list.clear()
             else:
-              print(f"Avg too low")
+              #print(f"Avg too low")
               macd_list_temp.clear()
               rsi_list_temp.clear()
+          else:
+            #print(f"Nothing in our holdings, clearing rsi and macd temp list")
+            macd_list_temp.clear()
+            rsi_list_temp.clear()
         else:
-          macd_list_temp.clear()
-          rsi_list_temp.clear()
-      else:
-        macd_list_temp.clear()
-        rsi_list_temp.clear()
-    else:
-      print(f"Incorrect test for macd {macd} and rsi {rsi}") 
+          #print(f"No downward MACD trend yet, keeping rsi and appending macd {macd} to macd list")
+          macd_list_temp.append(macd)
+          #rsi_list_temp.clear()
+      #else:
+        #print(f"Nothing to do")
+    #else:
+      #print(f"Incorrect test for macd {macd} and rsi {rsi}") 
+  time.sleep(2)
