8de66512b56bd77f698c6a7b0eab47e37f27736d
Tim Kuiper <tim.kuiper@protonmail.com>
2024-10-01 18:32:53 +0200

    upd

--- run_screens.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir screen_logs
-for file in $(ls | grep .py); do
-  screen -L -Logfile screen_logs/$file.txt -d -m -S $file python $file && echo "screen $file launched"
-  sleep 10
-done
